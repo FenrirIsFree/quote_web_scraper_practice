3dbb7b774176e4a1de79403e9be7cd0e807e99dd
Nick Lopez <116652056+FenrirIsFree@users.noreply.github.com>
2024-06-23 09:11:18 -0400

    Collected quote information and next button URL

---
+++ quote_scraper.py
@@ -0,0 +1,17 @@
+# https://quotes.toscrape.com/
+import requests
+from bs4 import BeautifulSoup
+
+all_quotes = []
+res = requests.get('https://quotes.toscrape.com/')
+soup = BeautifulSoup(res.text, 'html.parser')
+quotes = soup.find_all(class_='quote')
+
+for quote in quotes:
+    all_quotes.append({
+        'text': quote.find(class_='text').get_text(),
+        'author': quote.find(class_='author').get_text(),
+        'bio-link': quote.find('a')['href']
+    })
+
+next_button = soup.find(class_='next')
